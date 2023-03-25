31e91e5f7eca1e2b8780230a141aedf08abbf535
pierog <pgarcia@usmpvirtual.edu.pe>
2023-03-24 23:15:43 -0500

    pc1_galindo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace pc_galindopiero.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
